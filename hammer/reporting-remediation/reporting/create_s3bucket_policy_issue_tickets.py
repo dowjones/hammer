3e87bf9f2fc492845541348ffba96ade5544415c
Oleg Palij <o.palij@gmail.com>
2018-08-06 15:26:31 +0300

    use module accounts for reporting (not global)

--- hammer/reporting-remediation/reporting/create_s3bucket_policy_issue_tickets.py
+++ hammer/reporting-remediation/reporting/create_s3bucket_policy_issue_tickets.py
@@ -31,7 +31,7 @@ class CreateS3BucketPolicyIssueTickets:
         jira = JiraReporting(self.config)
         slack = SlackNotification(self.config)
 
-        for account_id, account_name in self.config.aws.accounts.items():
+        for account_id, account_name in self.config.s3policy.accounts.items():
             logging.debug(f"Checking '{account_name} / {account_id}'")
             issues = IssueOperations.get_account_not_closed_issues(ddb_table, account_id, S3PolicyIssue)
             for issue in issues:
