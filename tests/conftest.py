998836057d66979b9c2c54962ba9c59fa9bd2fd6
vigneswararaomacharla <Vigneswararao.Macharla@dowjones.com>
2019-01-21 19:52:36 +0530

    Reverting code changes.
    Reverting code changes.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,11 @@ import logging
 
 
 from library.logger import set_logging
-#from moto import mock_sts
+from moto import mock_sts
 
 
 def pytest_sessionstart(session):
     if session.config.option.verbose > 2:
         set_logging(level=logging.DEBUG) #, logfile="tests.log")
 
-    #   mock_sts().start()
+    mock_sts().start()
