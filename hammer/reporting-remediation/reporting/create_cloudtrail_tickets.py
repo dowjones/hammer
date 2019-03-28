ac13d063ca5ce94f4e97fa2fd7687db9aaf7ac43
MrBakalo <igor.bakalo@dowjones.com>
2019-03-28 17:34:13 +0200

    Added capability to disable/enable jira and slack reporting separately for each feature

--- hammer/reporting-remediation/reporting/create_cloudtrail_tickets.py
+++ hammer/reporting-remediation/reporting/create_cloudtrail_tickets.py
@@ -157,6 +157,10 @@ if __name__ == '__main__':
     module_name = sys.modules[__name__].__loader__.name
     set_logging(level=logging.DEBUG, logfile=f"/var/log/hammer/{module_name}.log")
     config = Config()
+    if config.jira.enabled:
+        config.jira.enabled = config.cloudtrails.jira.enabled
+    if config.slack.enabled:
+        config.slack.enabled = config.cloudtrails.slack.enabled
     add_cw_logging(config.local.log_group,
                    log_stream=module_name,
                    level=logging.DEBUG,
