a49dab10e97d438761ce272021683e4d9ef27c96
vigneswararaomacharla <Vigneswararao.Macharla@dowjones.com>
2019-05-06 18:44:58 +0530

    Updated with account based remediation changes.
    Updated with account based remediation changes.

--- hammer/reporting-remediation/remediation/clean_sqs_policy_permissions.py
+++ hammer/reporting-remediation/remediation/clean_sqs_policy_permissions.py
@@ -32,7 +32,7 @@ class CleanSQSPolicyPermissions:
         jira = JiraReporting(self.config)
         slack = SlackNotification(self.config)
 
-        for account_id, account_name in self.config.aws.accounts.items():
+        for account_id, account_name in self.config.sqspolicy.remediation_accounts.items():
             logging.debug(f"Checking '{account_name} / {account_id}'")
             issues = IssueOperations.get_account_open_issues(ddb_table, account_id, SQSPolicyIssue)
             for issue in issues:
